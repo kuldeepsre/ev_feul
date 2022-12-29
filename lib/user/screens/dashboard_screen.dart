e741a35342aa30e2f0c610a3a55166667d8ef327
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2022-12-29 17:43:53 +0530

    fix the location and geo locations issues  and adreess

--- lib/user/screens/dashboard_screen.dart
+++ lib/user/screens/dashboard_screen.dart
@@ -6,21 +6,10 @@ import 'package:ev_feul/utils/text_style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:geocoding/geocoding.dart';
 import '../mannage_patroller.dart';
-
-
-
-import 'package:flutter/material.dart';
-
-import 'package:geolocator/geolocator.dart';
-
-
-
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
-
   @override
   _GateScreenState createState() => _GateScreenState();
 }
